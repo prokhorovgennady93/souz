ca6c0b5145740cc58c03fa78206b9d3ce9376677
Developer <developer@example.com>
2026-04-10 17:14:52 +0300

    Auto-deploy update

---
+++ deploy.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Starting deployment to GitHub and Ubuntu Server...
+git add .
+git commit -m "Auto-deploy update"
+git push
+
+echo.
+echo Connecting to Ubuntu Server (192.168.2.137)...
+echo Password is: 3ghZ3Z32
+echo.
+ssh -t grevelien@192.168.2.137 "if [ ! -d ~/souz ]; then echo 'Cloning repository...' && git clone https://github.com/prokhorovgennady93/souz.git ~/souz; fi; cd ~/souz && git pull && sudo docker-compose up -d --build --remove-orphans"
+
+echo.
+echo Deployment finished!
+pause
